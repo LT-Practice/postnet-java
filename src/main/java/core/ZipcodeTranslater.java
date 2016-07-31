713a16f23ee7c2d221fbc2e44a9d665e5243d717
Leonard LiuTao <364125168@qq.com>
2016-08-01 00:40:29 +0800

    matchBarcode is finished

--- src/main/java/core/ZipcodeTranslater.java
+++ src/main/java/core/ZipcodeTranslater.java
@@ -1,6 +1,7 @@
 package core;
 
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,43 +10,121 @@ import java.util.regex.Pattern;
 
 public class ZipcodeTranslater {
 
+    //#5
     JSONObject checkZipcode(String zipcode) throws JSONException {
 
         JSONObject obj = new JSONObject();
-//        obj.put("balance", 123);
-//        JSONObject expected2 = new JSONObject("{'balance':123}");
 
         String formatCode = zipcode.replace("-", "");
         boolean onlyOneHyphen = zipcode.indexOf("-") == zipcode.lastIndexOf("-");
         Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
         boolean isNumber = pattern.matcher(formatCode).matches();
 
-        boolean type;
         if (formatCode.length() == 5 || formatCode.length() == 9 && onlyOneHyphen && isNumber) {
             obj.put("zipcode", formatCode);
-            obj.put("type", new Boolean(true));
+            obj.put("type", Boolean.TRUE);
         } else {
             obj.put("zipcode", zipcode);
-            obj.put("type", new Boolean(false));
+            obj.put("type", Boolean.FALSE);
         }
+
         return obj;
-
-
-//        return ;
     }
+
+    //#6-1
+    String formatZipcode(JSONObject checkedZipcode) throws JSONException {
+        String formattedZipcode = checkedZipcode.getString("zipcode").replace("-", "");
+
+        return formattedZipcode;
+    }
+
+    //#6-2
+    String[] buildZipcodeArray(String formattedZipcode) {
+        return formattedZipcode.split("");
+
+    }
+
+    //#6-3
+    String[] addCheckDigit(String[] zipcodeArray) {
+//        int[] zipcodeNumbers = new int[];
+        int sumResult = 0;
+        for (int i = 0; i < zipcodeArray.length; i++) {
+            sumResult += Integer.parseInt(zipcodeArray[i]);
+        }
+
+        String checkDigit;
+        if (sumResult % 10 != 0) {
+            checkDigit = String.valueOf((10 - sumResult % 10));
+        } else {
+            checkDigit = String.valueOf((0));
+        }
+        String[] zipcodeArrayWithCheckDigit = new String[zipcodeArray.length + 1];
+        for (int i = 0; i < zipcodeArray.length; i++) {
+            zipcodeArrayWithCheckDigit[i] = zipcodeArray[i];
+        }
+        zipcodeArrayWithCheckDigit[zipcodeArray.length] = checkDigit;
+
+        return zipcodeArrayWithCheckDigit;
+    }
+
+    //#6-4
+    String[] matchBarcode(String[] zipcodeArrayWithCheckDigit) throws JSONException {
+//        {'key': '0', 'digit': '||:::'},
+//        {'key': '1', 'digit': ':::||'},
+//        {'key': '2', 'digit': '::|:|'},
+//        {'key': '3', 'digit': '::||:'},
+//        {'key': '4', 'digit': ':|::|'},
+//        {'key': '5', 'digit': ':|:|:'},
+//        {'key': '6', 'digit': ':||::'},
+//        {'key': '7', 'digit': '|:::|'},
+//        {'key': '8', 'digit': '|::|:'},
+//        {'key': '9', 'digit': '|:|::'},
+        JSONArray obj = new JSONArray("[" +
+                "{'key': '0', 'digit': '||:::'},\n" +
+                "{'key': '1', 'digit': ':::||'},\n" +
+                "{'key': '2', 'digit': '::|:|'},\n" +
+                "{'key': '3', 'digit': '::||:'},\n" +
+                "{'key': '4', 'digit': ':|::|'},\n" +
+                "{'key': '5', 'digit': ':|:|:'},\n" +
+                "{'key': '6', 'digit': ':||::'},\n" +
+                "{'key': '7', 'digit': '|:::|'},\n" +
+                "{'key': '8', 'digit': '|::|:'},\n" +
+                "{'key': '9', 'digit': '|:|::'}]");
+        String s = obj.getJSONObject(1).getString("key");
+        String[] barcodeArray = new String[zipcodeArrayWithCheckDigit.length];
+//        for (int i = 0; i < zipcodeArrayWithCheckDigit.length; i++) {
+//            for (int j = 0; j < obj.length(); j++) {
+//                System.out.println(zipcodeArrayWithCheckDigit[i]);
+//                if (obj.getJSONObject(j).getString("key") == zipcodeArrayWithCheckDigit[i]) {
+//                    barcodeArray[i] = obj.getJSONObject(i).getString("digit");
+//                }
+//            }
+//            System.out.println(barcodeArray[i]);
+//        }
+        for (int i = 0; i < zipcodeArrayWithCheckDigit.length; i++) {
+            for (int j = 0; j < obj.length(); j++) {
+                if (obj.getJSONObject(j).getString("key").contains(zipcodeArrayWithCheckDigit[i])) {
+//                    System.out.println(obj.getJSONObject(j).getString("digit"));
+                    barcodeArray[i] = obj.getJSONObject(j).getString("digit");
+                }
+            }
+//            System.out.println(barcodeArray[i]);
+        }
+
+        return barcodeArray;
+    }
+
 
     public static void main(String[] args) throws JSONException {
-        JSONObject a = new ZipcodeTranslater().checkZipcode("45056-1234");
-//        JSONObject a = new ZipcodeTranslater().checkZipcode("450561234");
-        String zipcode = "450561234";
-        boolean onlyOneHyphen = zipcode.indexOf("-") == zipcode.lastIndexOf("-");
-        System.out.println(onlyOneHyphen);
+        String[] zipcodeArrayWithCheckDigit = {"5", "2", "3", "4", "5", "5"};
 
-        Pattern pattern = Pattern.compile("^[-\\+]?[\\d]*$");
-        boolean yes = pattern.matcher(zipcode).matches();
-        System.out.println(yes + "123");
-        System.out.println(a);
+        ZipcodeTranslater A = new ZipcodeTranslater();
+//        String s=A.matchBarcode(zipcodeArrayWithCheckDigit);
+//        System.out.println(s);
+        A.matchBarcode(zipcodeArrayWithCheckDigit);
     }
+
+    //
 
 
 }
--- src/test/java/core/ZipcodeTranslaterTest.java
+++ src/test/java/core/ZipcodeTranslaterTest.java
@@ -7,10 +7,82 @@ import static org.junit.Assert.*;
 
 
 public class ZipcodeTranslaterTest {
+    ZipcodeTranslater zipcodeTranslater = new ZipcodeTranslater();
+
+    @Test
+    public void matchBarcode() throws Exception {
+        String[] zipcodeArrayWithCheckDigit = {"1", "2", "3", "4", "5", "5"};
+
+        String[] barcodeArray = zipcodeTranslater.matchBarcode(zipcodeArrayWithCheckDigit);
+
+        String[] expected = {":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":|:|:"};
+
+        assertArrayEquals(expected, barcodeArray);
+    }
+
+
+
+
+    //6-3
+    @Test
+    public void addCheckDigit() throws Exception {
+        String[] zipcodeArray = {"1", "2", "3", "4", "5"};
+
+        String[] zipcodeArrayWithCheckDigit = zipcodeTranslater.addCheckDigit(zipcodeArray);
+
+        String[] expected = {"1", "2", "3", "4", "5", "5"};
+
+        assertArrayEquals(expected,zipcodeArrayWithCheckDigit);
+
+    }
+
+
+
+
+    //#6-2
+    @Test
+    public void buildZipcodeArray() throws Exception {
+        String formattedZipcode = "450561234";
+
+        String[] zipcodeArray = zipcodeTranslater.buildZipcodeArray(formattedZipcode);
+
+        String[] expected = {"4", "5", "0", "5", "6", "1", "2", "3", "4"};
+
+        assertArrayEquals(expected,zipcodeArray);
+
+    }
+
+
+    //#6-1
+    @Test
+    public void formatZipcode() throws Exception {
+        JSONObject checkedZipcode = new JSONObject("{'zipcode':'45056-1234','type':true}");
+
+        String formattedZipcode = zipcodeTranslater.formatZipcode(checkedZipcode);
+
+        String expected = "450561234";
+
+        assertEquals(expected,formattedZipcode);
+
+
+    }
+    @Test
+    public void formatZipcode2() throws Exception {
+        JSONObject checkedZipcode = new JSONObject("{'zipcode':'45056','type':true}");
+
+        String formattedZipcode = zipcodeTranslater.formatZipcode(checkedZipcode);
+
+        String expected = "45056";
+
+        assertEquals(expected,formattedZipcode);
+
+
+    }
+
+    //#5
     @Test
     public void checkZipcode() throws Exception {
         String zipcode = "45056-1234";
-        ZipcodeTranslater zipcodeTranslater = new ZipcodeTranslater();
 
         JSONObject checkedZipcode = zipcodeTranslater.checkZipcode(zipcode);
 //        String expected = "{\"zipcode\":\"45056-1234\",\"type\":true}";
@@ -57,6 +129,7 @@ public class ZipcodeTranslaterTest {
         assertEquals(expected.toString(), checkedZipcode.toString());
 
     }
+
     @Test
     public void checkZipcode5() throws Exception {
         String zipcode = "4505-";
@@ -69,6 +142,8 @@ public class ZipcodeTranslaterTest {
         assertEquals(expected.toString(), checkedZipcode.toString());
 
     }
+
+
 
 
 }
